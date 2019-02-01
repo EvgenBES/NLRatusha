526c791367814ed24bec5d250111b02301c6f6c8
Evgeniy <jekabut@gmail.com>
2019-02-01 20:42:15 +0300

    changed main activity xml

--- app/src/main/java/com/example/fox/ratusha/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/fox/ratusha/ui/main/MainActivity.kt
@@ -24,39 +24,46 @@ class MainActivity : BaseActivity(), MainView {
     }
 
     override fun showProgress() {
-        item_contain_wrapper.visibility = View.GONE
         progress_bar.visibility = View.VISIBLE
     }
 
     override fun hideProgress() {
         progress_bar.visibility = View.GONE
-        item_contain_wrapper.visibility = View.VISIBLE
     }
 
     @SuppressLint("SetTextI18n")
-    override fun setForpostInfo(timeOrder: String, progressOrder: Int, urlProduct: String, timeProduct: String){
+    override fun setForpostInfo(timeOrder: String, progressOrder: Int, urlProduct: String, timeProduct: String) {
         time_order_forp.text = timeOrder
         progress_order_forp.text = "$progressOrder%"
-        Picasso.get().load("http://image.neverlands.ru/weapon/$urlProduct").placeholder(R.drawable.ic_hourglass).error(R.drawable.ic_cancel).into(product_item_forp)
         product_time_forp.text = timeProduct
+        Picasso.get()
+                .load("http://image.neverlands.ru/weapon/$urlProduct")
+                .placeholder(R.drawable.ic_hourglass)
+                .error(R.drawable.ic_cancel)
+                .into(product_item_forp)
     }
 
     @SuppressLint("SetTextI18n")
-    override fun setOctalInfo(timeOrder: String, progressOrder: Int, urlProduct: String, timeProduct: String){
+    override fun setOctalInfo(timeOrder: String, progressOrder: Int, urlProduct: String, timeProduct: String) {
         time_order_octal.text = timeOrder
         progress_order_octal.text = "$progressOrder%"
-        Picasso.get().load("http://image.neverlands.ru/weapon/$urlProduct").placeholder(R.drawable.ic_hourglass).error(R.drawable.ic_cancel).into(product_item_octal)
         product_time_octal.text = timeProduct
+        Picasso.get()
+                .load("http://image.neverlands.ru/weapon/$urlProduct")
+                .placeholder(R.drawable.ic_hourglass)
+                .error(R.drawable.ic_cancel)
+                .into(product_item_octal)
     }
 
-    fun bottomNavigation(){
+    fun bottomNavigation() {
         BottomNavigationView.OnNavigationItemSelectedListener { item ->
             when (item.itemId) {
-                R.id.navigation_home -> return@OnNavigationItemSelectedListener true
-                R.id.navigation_list -> {
+                R.id.menu_btn_home -> return@OnNavigationItemSelectedListener true
+                R.id.menu_btn_fp -> return@OnNavigationItemSelectedListener true
+                R.id.menu_btn_oct -> {
                     return@OnNavigationItemSelectedListener true
                 }
-                R.id.navigation_alert -> return@OnNavigationItemSelectedListener true
+                R.id.menu_btn_info -> return@OnNavigationItemSelectedListener true
             }
             false
         }
