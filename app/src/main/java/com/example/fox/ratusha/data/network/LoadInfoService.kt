0d22c8e3e956624bb72ebdcd89e7d37af666db6d
Evgeniy <jekabut@gmail.com>
2019-02-02 20:28:45 +0300

    changed service

--- app/src/main/java/com/example/fox/ratusha/data/network/LoadInfoService.kt
+++ app/src/main/java/com/example/fox/ratusha/data/network/LoadInfoService.kt
@@ -6,48 +6,66 @@ import com.example.fox.ratusha.ui.entity.Order
 import java.io.BufferedReader
 import java.io.InputStream
 import java.io.InputStreamReader
+import java.net.HttpURLConnection
 import java.net.URL
 import java.net.URLConnection
+import java.nio.charset.Charset
 
 
-class LoadInfoService : AsyncTask<Void, Void, String>() {
+class LoadInfoService : AsyncTask<Void, Void, Order>() {
 
-    private var forpOrder: Order? = null
-    private var octalOrder: Order? = null
+    val TAG = "LoadInfoService"
+
+    private lateinit var forpOrder: Order
+    private lateinit var octalOrder: Order
+
+    @JvmField
+    val cp1251: Charset = Charset.forName("Cp1251")
+
     private val URL_FORT = "http://service.neverlands.ru/info/cityhall_1.txt"
     private val URL_OCTAL = "http://service.neverlands.ru/info/cityhall_2.txt"
 
-    override fun doInBackground(vararg p0: Void?): String? {
+    override fun doInBackground(vararg p0: Void?): Order? {
+
+        Log.d(TAG, "doInBackground")
 
         //Load info nl service Forpost
+        val connection = URL(URL_FORT).openConnection() as HttpURLConnection
         try {
-            val forpostList = arrayListOf<String>()
-            val conn: URLConnection = URL(URL_FORT).openConnection()
-            conn.connect()
-            val intpStream: InputStream = conn.getInputStream()
-            val reader = BufferedReader(InputStreamReader(intpStream, "Cp1251"), 8)
-            var line: String? = null
-            while ({ line = reader.readLine(); line }() != null) {
-
-                line?.let {
-                    if (it.contains("order") && it.length < 10) {
-                        // order forpost finished
-                    }
-                    if (it.contains("Телепорт")) {
-                        // appeared teleport in ratusha forpost
-                    }
-                    forpostList.add(it)
-//                    Log.d("LoadInfoService", it)
-                }
-            }
-            intpStream.close()
-
-            forpOrder = respons(forpostList)
-
-        } catch (e: Exception) {
-            Log.d("LoadInfoService ", "Exception ${e.message}")
-            e.printStackTrace()
+            val data: String = connection.inputStream.bufferedReader(cp1251).readText()
+        } finally {
+            connection.disconnect()
         }
+
+
+//        try {
+//            val forpostList = arrayListOf<String>()
+//            val conn: URLConnection = URL(URL_FORT).openConnection()
+//            conn.connect()
+//            val intpStream: InputStream = conn.getInputStream()
+//            val reader = BufferedReader(InputStreamReader(intpStream, "Cp1251"), 8)
+//            var line: String? = null
+//            while ({ line = reader.readLine(); line }() != null) {
+//
+//                line?.let {
+//                    if (it.contains("order") && it.length < 10) {
+//                        // order forpost finished
+//                    }
+//                    if (it.contains("Телепорт")) {
+//                        // appeared teleport in ratusha forpost
+//                    }
+//                    forpostList.add(it)
+////                    Log.d("LoadInfoService", it)
+//                }
+//            }
+//            intpStream.close()
+//
+//            forpOrder = respons(forpostList)
+//
+//        } catch (e: Exception) {
+//            Log.d("LoadInfoService ", "Exception ${e.message}")
+//            e.printStackTrace()
+//        }
 
 
         //Load info nl service Octal
@@ -82,10 +100,15 @@ class LoadInfoService : AsyncTask<Void, Void, String>() {
         return null
     }
 
-    //Start after background to UI
-    override fun onPostExecute(result: String?) {
+    override fun onPostExecute(result: Order?) {
         super.onPostExecute(result)
+
+        if (result != null) {
+            Log.d(TAG, "onPostExecute 1 ${result.dataOrder[0]} ${result.dataOrder[1]} ${result.dataOrder[2]}")
+
+        }
     }
+
 
     fun respons(forpostList: ArrayList<String>): Order {
         val responseOrder = Order(forpostList, forpostList, forpostList)
@@ -106,8 +129,6 @@ class LoadInfoService : AsyncTask<Void, Void, String>() {
         }
         return responseOrder
     }
-
-
 
 
 }
--- app/src/main/java/com/example/fox/ratusha/service/LoadService.kt
+++ app/src/main/java/com/example/fox/ratusha/service/LoadService.kt
@@ -2,7 +2,9 @@ package com.example.fox.ratusha.service
 
 import android.app.Service
 import android.content.Intent
+import android.os.Handler
 import android.os.IBinder
+import android.util.Log
 import com.example.fox.ratusha.data.network.LoadInfoService
 import java.util.concurrent.TimeUnit
 
@@ -12,26 +14,30 @@ class LoadService : Service() {
     val TAG = "LoadInfoService"
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
+        Log.d(TAG, "onStartCommand")
+
         someTask()
         return super.onStartCommand(intent, flags, startId)
     }
 
     override fun onCreate() {
         super.onCreate()
+
+        Log.d(TAG, "onCreate")
     }
 
     override fun onBind(p0: Intent?): IBinder? {
         return null
+
+
     }
 
-    fun someTask() {
+    private fun someTask() {
+
         Thread(Runnable {
             while (true) {
-                try {
-                    TimeUnit.SECONDS.sleep(300)
-                } catch (e: InterruptedException) {
-                    e.printStackTrace()
-                }
+                TimeUnit.SECONDS.sleep(5L)
                 LoadInfoService().execute()
             }
 //            stopSelf()
